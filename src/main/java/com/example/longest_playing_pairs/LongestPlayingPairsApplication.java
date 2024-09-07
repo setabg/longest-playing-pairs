e4e227e1a825543d8aa51e967af5a369d93ff4a2
@cveti <svetlanastefanova5@gmail.com>
2024-09-07 10:37:11 +0300

    Initial commit

---
+++ src/main/java/com/example/longest_playing_pairs/LongestPlayingPairsApplication.java
@@ -0,0 +1,13 @@
+package com.example.longest_playing_pairs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LongestPlayingPairsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LongestPlayingPairsApplication.class, args);
+	}
+
+}
